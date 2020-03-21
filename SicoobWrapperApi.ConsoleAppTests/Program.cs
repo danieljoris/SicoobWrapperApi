3dec353c0ecdfe687c6211e8f4be615e624a7f63
daniel.joris <daniel.joris@tryideas.com.br>
2020-03-20 21:55:14 -0300

    Adicionar arquivos de projeto.

---
+++ SicoobWrapperApi.ConsoleAppTests/Program.cs
@@ -0,0 +1,20 @@
+﻿using Refit;
+using SicoobWrapperApi.BankCharges.Entities;
+using SicoobWrapperApi.BankCharges.Interfaces;
+using System;
+
+namespace SicoobWrapperApi.ConsoleAppTests
+{
+    class Program
+    {
+        async static void Main(string[] args)
+        {
+            var sicoobApi = RestService.For<IBoleto>("https://sandbox.sicoob.com.br/cobranca-bancaria/");
+
+            var boleto = new Boleto();
+
+            var teste = await sicoobApi.AddBoleto(boleto);
+
+        }
+    }
+}
---
+++ SicoobWrapperApi/Entities/Guarantor.cs
@@ -0,0 +1,24 @@
+﻿using Refit;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SicoobWrapperApi.BankCharges.Entities
+{
+    public class Guarantor
+    {
+        /// <summary>
+        /// CPF ou CNPJ do Sacador avalista. Tamanho máximo 14
+        /// (opcional)
+        /// </summary>
+        [AliasAs("numeroCpfCnpjSacadorAvalista")]
+        public string CpfCnpjNumber { get; private set; }
+
+        /// <summary>
+        /// Nome do Sacador avalista.
+        /// (opcional)
+        /// </summary>
+        [AliasAs("nomeSacadorAvalista")]
+        public string Name { get; private set; }
+    }
+}
